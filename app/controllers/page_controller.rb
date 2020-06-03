7e93a53dfa8c7d5c448e3292bdadbf7cc0bc39be
Dan Vera <arubinofaux@gmail.com>
2020-06-03 03:51:04 -0400

    wip

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,14 +1,14 @@
 class PageController < ApplicationController
   def home
-    @mods = Mod.where(mod_type: "module").order('updated_at DESC')
+    @mods = Mod.where(mod_type: "module").order('repo_updated_at DESC')
   end
 
   def tools
-    @mods = Mod.where(mod_type: "tool").order('updated_at DESC')
+    @mods = Mod.where(mod_type: "tool").order('repo_updated_at DESC')
   end
 
   def lua
-    @mods = Mod.where(mod_type: "lua-script").order('updated_at DESC')
+    @mods = Mod.where(mod_type: "lua-script").order('repo_updated_at DESC')
   end
 
   def search
